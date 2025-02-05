e95d930ded4d4e8040abfbfe0112b043f37e0c16
andres632425 <bootcampdev2425@gmail.com>
2025-02-05 13:03:49 +0100

    Primera estructura de vue

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1 :class="titular">Esto es un archivo principal para {{ valor }}</h1>
+    <button @:click="cambiar">{{ mensaje }}</button>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+
+const valor = "El Bootcamp";
+let titular = ref("oscuro");
+let mensaje = ref("CLARO");
+const cambiar = () => {
+  if (titular.value == "oscuro") {
+    titular.value = "claro";
+    mensaje.value = "OSCURO";
+  } else {
+    titular.value = "oscuro";
+    mensaje.value = "CLARO";
+  }
+};
+</script>
+
+<style scoped>
+.oscuro {
+  background-color: blue;
+  color: azure;
+}
+
+.claro {
+  background-color: rgb(190, 237, 172);
+  color: rgb(43, 77, 77);
+}
+</style>
